bdc5b290b66234959344303c1bd861ca23f95110
MUedsa <7676275+muedsa@users.noreply.github.com>
2022-05-19 02:44:07 +0800

    fix: use android play url

--- BilibiliLiveApiClient/src/main/java/com/muedsa/bilibililiveapiclient/ApiUrlContainer.java
+++ BilibiliLiveApiClient/src/main/java/com/muedsa/bilibililiveapiclient/ApiUrlContainer.java
@@ -1,7 +1,7 @@
 package com.muedsa.bilibililiveapiclient;
 
 public class ApiUrlContainer {
-    public static String ROOM_PLAY_URL = "https://api.live.bilibili.com/room/v1/Room/playUrl?cid=%d&qn=%d&platform=web";
+    public static String ROOM_PLAY_URL = "https://api.live.bilibili.com/room/v1/Room/playUrl?cid=%d&qn=%d&platform=android";
 
     public static String GET_ROOM_LIST = "https://api.live.bilibili.com/room/v3/area/getRoomList?page=%d&page_size=%d&parent_area_id=%d&sort_type=%s";
 
