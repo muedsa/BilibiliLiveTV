0d757ae211eda51fda8a751206241cc5bab78b81
MUedsa <7676275+muedsa@users.noreply.github.com>
2022-07-06 14:16:13 +0800

    update: remove invalid import

--- BilibiliLiveApiClient/src/main/java/com/muedsa/bilibililiveapiclient/uitl/ChatBroadcastPacketUtil.java
+++ BilibiliLiveApiClient/src/main/java/com/muedsa/bilibililiveapiclient/uitl/ChatBroadcastPacketUtil.java
@@ -1,21 +1,10 @@
 package com.muedsa.bilibililiveapiclient.uitl;
 
-import com.alibaba.fastjson.JSONObject;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.zip.Inflater;
-
-import sun.nio.ByteBuffered;
 
 public class ChatBroadcastPacketUtil {
 
