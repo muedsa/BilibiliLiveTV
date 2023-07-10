fb79315c778d8e3148a305cd2a5f2ab996455792
MUEDSA <7676275+muedsa@users.noreply.github.com>
2023-07-10 10:54:45 +0800

    remove: version legacy

--- app/src/main/java/com/muedsa/bilibililivetv/App.java
+++ app/src/main/java/com/muedsa/bilibililivetv/App.java
@@ -8,12 +8,8 @@ import com.google.common.base.Strings;
 import com.muedsa.bilibililivetv.container.BilibiliLiveApi;
 import com.muedsa.bilibililivetv.preferences.Prefs;
 import com.muedsa.bilibililivetv.room.AppDatabase;
-import com.muedsa.bilibililivetv.util.VersionLegacy;
 
 import java.util.Map;
-
-import io.reactivex.rxjava3.core.Completable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class App extends Application {
 
@@ -23,10 +19,6 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         database = AppDatabase.getDatabase(this);
-        Completable.create(emitter -> {
-            VersionLegacy.roomLiveHistory(App.this, database.getLiveRoomDaoWrapper());
-            emitter.onComplete();
-        }).subscribeOn(Schedulers.io()).subscribe();
         Prefs.init(getApplicationContext());
         String json = Prefs.getString(Prefs.BILIBILI_COOKIE_JSON);
         if (!Strings.isNullOrEmpty(json)) {
