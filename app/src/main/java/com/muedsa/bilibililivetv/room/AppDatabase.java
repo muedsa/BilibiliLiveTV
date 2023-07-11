d98b36c3e93fb5e9c3c525b60851be6b4b3831bc
MUEDSA <7676275+muedsa@users.noreply.github.com>
2023-07-11 13:02:57 +0800

    update: room database version 2

--- app/src/main/java/com/muedsa/bilibililivetv/room/AppDatabase.java
+++ app/src/main/java/com/muedsa/bilibililivetv/room/AppDatabase.java
@@ -13,7 +13,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 import com.muedsa.bilibililivetv.room.dao.LiveRoomDao;
 import com.muedsa.bilibililivetv.room.model.LiveRoom;
 
-@Database(entities = {LiveRoom.class}, version = 1, exportSchema = false)
+@Database(entities = {LiveRoom.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static final String TAG = AppDatabase.class.getSimpleName();
